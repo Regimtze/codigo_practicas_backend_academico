3608b47b0cb875b87db20642b1b41c4b204c6df0
Regina <reginamtzesp07@gmail.com>
2026-09-24 11:44:07 -0600

    Add files via upload

---
+++ U01/inicio/src/main/java/mx/edu/backendacademico/BackendAcademicoApplication.java
@@ -0,0 +1,11 @@
+package mx.edu.backendacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAcademicoApplication {
+    public static void main (String[] args){
+        SpringApplication.run(BackendAcademicoApplication.class, args);
+    }
+}
