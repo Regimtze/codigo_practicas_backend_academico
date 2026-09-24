d2ab53fe0a45c8606da0ec841cb598a65b94130b
Regina <reginamtzesp07@gmail.com>
2026-09-24 12:06:28 -0600

    Add files via upload

---
+++ U01/inicio/src/test/java/mx/edu/backendacademico/BackendAcademicoApplicationTest.java
@@ -0,0 +1,11 @@
+package mx.edu.backendacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BackendAcademicoApplicationTest {
+    @Test
+    void contextLoads(){
+    }
+}
